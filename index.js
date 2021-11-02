5f128479586845e0b4448560864687de2bd2c859
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-11-02 01:30:51 -0600

    accesibilidad terminanda, hoy repaso

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", validateForm);
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
   document.body.addEventListener("click", e => closeModal(e));
+  document.body.addEventListener("keyup", e => listenForScape(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -69,17 +70,39 @@ function clickLeft() {
   }
 }
 
+function validateForm(e) {
+  e.preventDefault();
+  const nameField = document.querySelector("#name");
+  if(nameField.value === '') {
+    document.getElementById("name-error").textContent = "!Please enter your name";
+  } else {
+    showNotification();
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.querySelector("#name-error").textContent = "";
+  document.querySelector(".form-container").reset();
+  document.querySelector(".notification").innerHTML = "El formulario fue enviado sin errores";
   document.querySelector(".notification").style.display = "flex";
   setTimeout(function() {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
 }
 
+// listen scape key
+function listenForScape(e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
+  console.log('focus')
+  document.getElementById("modal-header").focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
